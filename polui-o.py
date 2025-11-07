9b84495c9a60a41186edc3c3198004a8782e5ca8
pedrohenriquedeaquinogodoydeso-stack <pedrohenriquedeaquinogodoydeso@gmail.com>
2025-11-07 16:57:03 -0300

    Update polui-o.py

--- polui-o.py
+++ polui-o.py
@@ -1,1 +1,30 @@
+import discord
+from discord.ext import commands
+import random
+import requests
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+#a = input('qual veículo você vai utilizar?')
+#km = input('quantos quilômetros serão rodados')
+#veiculodict= {'carro':120 , 'moto':50}
+#if a == 'carro':
+#   return 120 * b
+#elif a == 'moto':
+#  50 * b
+@bot.command()
+async def pokemon(ctx, ):
+    await ctx.send(f'{pokemon_image}')
+    view = discord.ui.View()
+    botao = discord.ui.Button(label='capturar', style=discord.ButtonStyle.green)
+    botao.callback = resposta_botao
+    botao2 = discord.ui.Button(label='fugir', style=discord.ButtonStyle.green)
+    botao2.callback = resposta_botao2
+
+    view.add_item(botao2)
+    view.add_item(botao)
+    await ctx.reply(view=view)
+
+bot.run('TOLKEN')
